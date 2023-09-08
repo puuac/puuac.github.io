afee4594d93c7bfcf348406e369944efde67a1c4
puuac <144440917+puuac@users.noreply.github.com>
2023-09-08 19:34:06 +0800

    Update d1.js

--- din/d1.js
+++ din/d1.js
@@ -9,7 +9,7 @@ varconf = {  _
 	"adth1": "分享好友后获得+5次的刷新机会,>"\"width:90%;margin-top:13px;border-radius:5px;\" style=\"images/here.png\"
 	"adthe": "分享好友后获得+5次的刷新机会,
 	“标题”：[
-		"全网最新精品合集"
+		"✽全网最新精品合集✽"
 	] ,
 	"topad" : "https://sharechain.qq.com/5c3750025a17bd97d5bc64fce6497715#?{nnnnnnnn}{www}" ,
 	"sInfo": "没有观看次数了！\r\n①请复制转发到Q群 增加观看次数\r\n②每有一人打开你就增加5次\r\n③没有人打开不增加次数",
